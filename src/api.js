a94f6eaf5ea04da216eace0d5bfedbe00f99c545
mohamedazab <mohamedazab1996@gmail.com>
2020-06-05 21:37:45 +0200

    add Starter Code

---
+++ src/api.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const express    = require('express');
+const bodyParser = require('body-parser');
+const helmet     = require('helmet');
+
+const middlewares = require('./middlewares');
+
+const auth  = require('./routes/auth');
+
+const api = express();
+
+
+// Adding Basic Middlewares
+api.use(helmet());
+api.use(bodyParser.json());
+api.use(bodyParser.urlencoded({ extended: false }));
+api.use(middlewares.allowCrossDomain);
+
+
+// Basic route
+api.get('/', (req, res) => {
+    res.json({
+        name: 'Welcome to Switch Space backend'
+    });
+});
+
+// API routes
+api.use('/auth'  , auth);
+
+
+
+module.exports = api;
