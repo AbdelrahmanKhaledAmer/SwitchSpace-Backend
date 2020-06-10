e439654495734a0b57eda42b327919f505c483f3
mohamedazab <mohamedazab1996@gmail.com>
2020-06-10 22:23:08 +0200

    feat: add login validation

--- src/models/validations/userRegister.js
+++ src/models/validations/userRegister.js
@@ -1,19 +1,17 @@
 const Joi = require("@hapi/joi");
 
-const userSchemaValidator = Joi.object({
-  username: Joi.string().alphanum().min(3).max(30).required(),
-
+const registerValidator = Joi.object({
   email: Joi.string()
     .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
     .required(),
 
   // use simple password for now and use strong password after developing the BE
   // new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
-  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
+  password: Joi.string().required(),
 
   repeatPassword: Joi.ref("password"),
 
   name: Joi.string().alphanum().min(2).max(50).required(),
 }).with("password", "repeatPassword");
 
-module.exports = userSchemaValidator;
+module.exports = registerValidator;
