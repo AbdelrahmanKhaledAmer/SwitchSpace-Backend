5d9d07f74fa4f55595e60a47f996ba14169c91f7
mohamedazab <mohamedazab1996@gmail.com>
2020-06-11 11:33:00 +0200

    add logger to the server

--- src/routes/userAuth.js
+++ src/routes/userAuth.js
@@ -2,14 +2,16 @@
 
 const express = require("express");
 const router = express.Router();
+const logger = require("../middlewares/loggerHandler");
 
-const middlewares = require("../middlewares");
+const middlewares = require("../middlewares/middlewares");
 const UserAuthController = require("../controllers/userAuth");
 
-router.post("/login", UserAuthController.login);
-router.post("/register", UserAuthController.register);
+router.post("/login", logger, UserAuthController.login);
+router.post("/register", logger, UserAuthController.register);
 router.get(
   "/logout",
+  logger,
   middlewares.checkAuthentication,
   UserAuthController.logout
 );
