b48e8190b752a2e69ddbf74f82fa7fc9b2df649a
mohamedazab <mohamedazab1996@gmail.com>
2020-06-11 11:34:35 +0200

    remove service

--- src/middlewares/loggerHandler.js
+++ src/middlewares/loggerHandler.js
@@ -2,7 +2,7 @@ const winston = require("winston");
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.json(),
-  defaultMeta: { service: "user-service" },
+  // defaultMeta: { service: "user-service" },
   transports: [
     //
     // - Write all logs with level `error` and below to `error.log`
