0e81d8e218a19a4141fbbefded3a15dc32fe4a6b
mohamedazab <mohamedazab1996@gmail.com>
2020-06-11 22:58:43 +0200

    fix: not allow email update

--- src/models/validations/userUpdate.js
+++ src/models/validations/userUpdate.js
@@ -1,11 +1,6 @@
 const Joi = require("@hapi/joi");
 
 const registerValidator = Joi.object({
-  email: Joi.string().email({
-    minDomainSegments: 2,
-    tlds: { allow: ["com", "net"] },
-  }),
-
   // use simple password for now and use strong password after developing the BE
   // new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
   password: Joi.string(),
