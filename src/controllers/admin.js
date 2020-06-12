9cdad5226df937ac486fc701ac74c0cbdc60f58d
AbdelrahmanKhaledAmer <abdelrahmanka96@gmail.com>
2020-06-12 16:40:02 +0200

    feat: add delete report untested fix: wrong status code in view reports

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -5,9 +5,25 @@ const ReportModel = require("../models/schema/report");
 // Get all reports with pagination
 const viewAllReports = async (req, res) => {
   try {
-    let reports = ReportModel.find({}).limit(10);
-    return res.status(500).json({
+    let reports = await ReportModel.find({});
+    return res.status(200).json({
       data: reports,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: "Internal server error.",
+    });
+  }
+};
+
+// ********************************************************************************************************* //
+
+// Delete a report
+const deleteReport = async (req, res) => {
+  try {
+    await ReportModel.deleteOne({ _id: req.body.reportId });
+    return res.status(200).json({
+      data: { message: "Report deleted successfully" },
     });
   } catch (err) {
     return res.status(500).json({
@@ -18,4 +34,5 @@ const viewAllReports = async (req, res) => {
 
 module.exports = {
   viewAllReports,
+  deleteReport,
 };
