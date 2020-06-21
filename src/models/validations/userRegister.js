4bbf97bfacc5bce4c879a4f0b60d955a0f3444bc
AbdelrahmanKhaledAmer <abdelrahmanka96@gmail.com>
2020-06-21 18:17:21 +0200

    fix: user seed and token has user name

--- src/models/validations/userRegister.js
+++ src/models/validations/userRegister.js
@@ -1,4 +1,14 @@
 const Joi = require("@hapi/joi");
+const passwordComplexity = require("joi-password-complexity");
+const passwordComplexityOpts = {
+  min: 8,
+  max: 50,
+  lowerCase: 1,
+  upperCase: 1,
+  numeric: 1,
+  symbol: 1,
+  requirementCount: 4,
+};
 
 const registerValidator = Joi.object({
   email: Joi.string()
@@ -7,7 +17,7 @@ const registerValidator = Joi.object({
 
   // use simple password for now and use strong password after developing the BE
   // new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
-  password: Joi.string().required(),
+  password: passwordComplexity(passwordComplexityOpts),
 
   repeatPassword: Joi.ref("password"),
 
