ad34374c30fe2cd032c6ed34d0618906e276ae5e
mohamedazab <mohamedazab1996@gmail.com>
2020-06-21 14:12:08 +0200

    fix: .env file not working

--- src/config.js
+++ src/config.js
@@ -5,9 +5,12 @@ const port = process.env.PORT || "3000";
 const mongoURI =
   process.env.MONGODB_URI || "mongodb://localhost:27017/switch_space";
 const JwtSecret = process.env.JWT_SECRET || "very secret secret";
-
+const AWSAccessKeyId = process.env.AWSAccessKeyId;
+const AWSSecretKey = process.env.AWSSecretKey;
 module.exports = {
   port,
   mongoURI,
   JwtSecret,
+  AWSAccessKeyId,
+  AWSSecretKey,
 };
