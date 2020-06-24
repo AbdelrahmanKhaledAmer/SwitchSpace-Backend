3b68a1f2cbd492e71d107879e4a065f6af089736
mohamedazab <mohamedazab1996@gmail.com>
2020-06-24 19:40:06 +0200

    Fix: update secret keys

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 "use strict";
 
 // Configuration variables
-const port = process.env.PORT || "3000";
-const mongoURI = process.env.MONGODB_URI || "mongodb://localhost:27017/switch_space";
-const JwtSecret = process.env.JWT_SECRET || "very secret secret";
+const port = process.env.PORT;
+const mongoURI = process.env.MONGODB_URI;
+const JwtSecret = process.env.JWT_SECRET;
 const AWSAccessKeyId = process.env.AWSAccessKeyId;
 const AWSSecretKey = process.env.AWSSecretKey;
 const STRIPE_PAYMENT = process.env.STRIPE_PAYMENT;
