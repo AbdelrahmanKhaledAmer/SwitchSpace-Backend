e4a47d604e2fac90534806ab88ae2cc12ba851b2
mohamedazab <mohamedazab1996@gmail.com>
2020-06-24 19:24:19 +0200

    env: add stripe key in config file

--- src/config.js
+++ src/config.js
@@ -6,10 +6,12 @@ const mongoURI = process.env.MONGODB_URI || "mongodb://localhost:27017/switch_sp
 const JwtSecret = process.env.JWT_SECRET || "very secret secret";
 const AWSAccessKeyId = process.env.AWSAccessKeyId;
 const AWSSecretKey = process.env.AWSSecretKey;
+const STRIPE_PAYMENT = process.env.STRIPE_PAYMENT;
 module.exports = {
     port,
     mongoURI,
     JwtSecret,
     AWSAccessKeyId,
     AWSSecretKey,
+    STRIPE_PAYMENT,
 };
