6ffff2d9e61f63249ce7fcd2dc832b3532ce0db6
mohamedAlmorsi <mohamed.almorsi.alshaer@gmail.com>
2020-06-30 16:26:21 +0200

    Fixed report issing validation

--- src/models/validations/report.js
+++ src/models/validations/report.js
@@ -2,6 +2,7 @@ const Joi = require("@hapi/joi");
 
 const reportSchemaValidator = Joi.object({
     reporterId: Joi.required(),
+    reporterName: Joi.string().require(),
     postId: Joi.required(),
     complaint: Joi.required(),
 });
