0c1abbfbd91b59b409fb0a408783e24072e0fe23
mohamedazab <mohamedazab1996@gmail.com>
2020-07-17 01:41:24 +0200

    feat: logger in categories controller

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -4,17 +4,19 @@ const mongoose = require("mongoose");
 const SubcategorySchema = require("../models/schema/subcategory");
 const SubcategoryModel = mongoose.model("Subcategory", SubcategorySchema);
 const CategoryModel = require("../models/schema/category");
+const loggerHandlers = require("../utils/logger/loggerHandlers")
 
 // ********************************************************************************************************* //
 
 // Returns all categories
-const getCategories = async (req, res) => {
+const getCategories = async (req, res) => {  
     try {
         let categories = await CategoryModel.find({});
         return res.status(200).json({
             data: categories,
         });
     } catch (err) {
+        loggerHandlers.errorHandler(err)
         return res.status(500).json({
             message: "Internal server error.",
         });
@@ -31,6 +33,7 @@ const getSubcategories = async (req, res) => {
             data: subcategories,
         });
     } catch (err) {
+        loggerHandlers.errorHandler(err)
         return res.status(500).json({
             message: "Internal server error.",
         });
@@ -47,6 +50,7 @@ const trendingCategories = async (req, res) => {
             data: subcategories,
         });
     } catch (err) {
+        loggerHandlers.errorHandler(err)
         return res.status(500).json({
             message: "Internal server error.",
         });
